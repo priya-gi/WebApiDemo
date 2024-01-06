b131275b27fc6aea5be9462f0e965d681b0870d8
Priyanka <Admin@DESKTOP-M0HRL1H>
2024-01-06 15:31:03 +0530

    Add project files.

---
+++ WebApiDemo/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiDemo.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [Route("api/priyaaction/{id}")]
+        public IHttpActionResult Get([FromUri]int id)
+        {
+            if(id > 5)
+            {
+				return Ok(1);
+			}
+            else
+            {
+				return NotFound();
+			}
+            
+        }
+    }
+}
